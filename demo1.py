0c24fb18f908528ed08a4320b553b38f083048c2
Ibrahim DJEMAI <39132283+IbraDje@users.noreply.github.com>
2018-06-17 15:04:33 +0100

    Test File

--- demo1.py
+++ demo1.py
@@ -1,8 +1,9 @@
-from PFCM import pfcm
+from PFCM import pfcm, pfcm_predict
 import numpy as np
 import matplotlib.pyplot as plt
 
 # the original test : https://pythonhosted.org/scikit-fuzzy/auto_examples/plot_cmeans.html#example-plot-cmeans-py
+
 colors = ['b', 'orange', 'g', 'r', 'c', 'm', 'y', 'k', 'Brown', 'ForestGreen']
 
 # Define three cluster centers
@@ -31,13 +32,15 @@ for label in range(3):
     ax0.plot(xpts[labels == label], ypts[labels == label], '.',
              color=colors[label])
 ax0.set_title('Test data: 200 points x3 clusters.')
+###############################################################################
 
 # Set up the loop and plot
 fig1, axes1 = plt.subplots(3, 3, figsize=(8, 8))
 alldata = np.vstack((xpts, ypts)).T
 
 for ncenters, ax in enumerate(axes1.reshape(-1), 2):
-    cntr, U, T, obj_fcn = pfcm(alldata, ncenters)
+    cntr, U, T, obj_fcn = pfcm(
+        alldata, ncenters)
 
     # Plot assigned clusters, for each data point in training set
     cluster_membership = np.argmax(U, axis=0)
@@ -53,3 +56,41 @@ for ncenters, ax in enumerate(axes1.reshape(-1), 2):
     ax.axis('off')
 
 fig1.tight_layout()
+###############################################################################
+# Regenerate model with 3 cluster centers - note that center ordering
+# is random in this clustering algorithm, so the centers may change places
+cntr, U, T, obj_fcn = pfcm(alldata, 3)
+
+# Show 3-cluster model
+fig2, ax2 = plt.subplots()
+ax2.set_title('Trained model')
+for j in range(3):
+    ax2.plot(alldata.T[0, np.argmax(U, axis=0) == j],
+             alldata.T[1, np.argmax(U, axis=0) == j], 'o',
+             label='series ' + str(j))
+ax2.legend()
+
+###############################################################################
+
+# Generate uniformly sampled data spread across the range [0, 10] in x and y
+newdata = np.random.uniform(0, 1, (1100, 2)) * 10
+
+# Predict new cluster membership with `cmeans_predict` as well as
+# `cntr` from the 3-cluster model
+U, T, obj_fcn = pfcm_predict(newdata, cntr)
+
+# Plot the classified uniform data. Note for visualization the maximum
+# membership value has been taken at each point (i.e. these are hardened,
+# not fuzzy results visualized) but the full fuzzy result is the output
+# from pfcm_predict.
+cluster_membership = np.argmax(U, axis=0)  # Hardening for visualization
+
+fig3, ax3 = plt.subplots()
+ax3.set_title('Random points classifed according to known centers')
+for j in range(3):
+    ax3.plot(newdata[cluster_membership == j, 0],
+             newdata[cluster_membership == j, 1], 'o',
+             label='series ' + str(j))
+ax3.legend()
+
+plt.show()
