86dec2d581ab71d6aec9e7843f8c8e0e16af27a3
Ibrahim DJEMAI <39132283+IbraDje@users.noreply.github.com>
2018-06-17 12:47:40 +0100

    Test File
    Testing the PFCM Algorithm on a 2D data points.
    The test is duplicated from the "skfuzzy" test of the FCM Algorithm

---
+++ testPFCM2.py
@@ -0,0 +1,60 @@
+from PFCM import pfcm
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+start = time.clock()
+colors = ['b', 'orange', 'g', 'r', 'c', 'm', 'y', 'k', 'Brown', 'ForestGreen']
+
+# Define three cluster centers
+centers = [[4, 2],
+           [1, 7],
+           [5, 6]]
+
+# Define three cluster sigmas in x and y, respectively
+sigmas = [[0.8, 0.3],
+          [0.3, 0.5],
+          [1.1, 0.7]]
+
+# Generate test data
+np.random.seed(42)  # Set seed for reproducibility
+xpts = np.zeros(1)
+ypts = np.zeros(1)
+labels = np.zeros(1)
+for i, ((xmu, ymu), (xsigma, ysigma)) in enumerate(zip(centers, sigmas)):
+    xpts = np.hstack((xpts, np.random.standard_normal(200) * xsigma + xmu))
+    ypts = np.hstack((ypts, np.random.standard_normal(200) * ysigma + ymu))
+    labels = np.hstack((labels, np.ones(200) * i))
+
+# Visualize the test data
+fig0, ax0 = plt.subplots()
+for label in range(3):
+    ax0.plot(xpts[labels == label], ypts[labels == label], '.',
+             color=colors[label])
+ax0.set_title('Test data: 200 points x3 clusters.')
+###############################################################################
+
+# Set up the loop and plot
+fig1, axes1 = plt.subplots(3, 3, figsize=(8, 8))
+alldata = np.vstack((xpts, ypts)).T
+
+for ncenters, ax in enumerate(axes1.reshape(-1), 2):
+    cntr, U, T, obj_fcn = pfcm(
+        alldata, ncenters)
+
+    # Plot assigned clusters, for each data point in training set
+    cluster_membership = np.argmax(U, axis=0)
+    for j in range(ncenters):
+        ax.plot(xpts[cluster_membership == j],
+                ypts[cluster_membership == j], '.', color=colors[j])
+
+    # Mark the center of each fuzzy cluster
+    for pt in cntr:
+        ax.plot(pt[0], pt[1], 'rs')
+
+    ax.set_title('Centers = {0}'.format(ncenters))
+    ax.axis('off')
+
+fig1.tight_layout()
+
+print('elapsed time = {}'.format(time.clock() - start))
