3469f3de9349697912716df27f78af404d8aabd6
Ibrahim DJEMAI <39132283+IbraDje@users.noreply.github.com>
2018-06-19 23:17:34 +0100

    Test File
    testing the PFCM Algorithm on a image

--- demo2.py
+++ demo2.py
@@ -5,6 +5,7 @@ import time
 
 
 def creat_image(labels, centers):
+    """ color each pixel with the cluster's center color that it belongs to """
     img = np.zeros(shape=(labels.shape[0], labels.shape[1], 3))
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
@@ -15,12 +16,12 @@ def creat_image(labels, centers):
     return img
 
 
-clusters = 4
-img = mpimg.imread('rainbow-page2.jpg') # test image
-start_time = time.clock()
+clusters = 4 # define the number of clusters
+img = mpimg.imread('rainbow-page2.jpg') # read the test image
+start_time = time.clock() # start calculating the execution time
 centers, U, T, obj_fcn = pfcm(
-    img.reshape(img.shape[0]*img.shape[1], img.shape[2]), clusters)
-elapsed_time = time.clock() - start_time
-labels = np.argmax(U, axis=0).reshape(img.shape[0], img.shape[1])
-creat_image(labels, centers)
-print(f'elapsed time : {round(elapsed_time, 3)} seconds')
+    img.reshape(img.shape[0]*img.shape[1], img.shape[2]), clusters) # calling the pfcm function on the image after reshaping it
+elapsed_time = time.clock() - start_time # end calculating the execution time
+labels = np.argmax(U, axis=0).reshape(img.shape[0], img.shape[1]) # assing each pixel to its closest cluster
+creat_image(labels, centers) # creat an image with the assigned clusters
+print(f'elapsed time : {round(elapsed_time, 3)} seconds') # printing the execution time
